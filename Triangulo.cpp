455022fcfa6e6ba3e61f5c7fd62e806675d8ec76
PedroValdezF <a00838331@tec.mx>
2024-05-14 14:32:01 -0600

    Update Triangulo.cpp

--- Triangulo.cpp
+++ Triangulo.cpp
@@ -33,9 +33,9 @@ void Triangulo::setV3(Punto _v3){
 }
 
 double Triangulo::perimetro(){
-    double peri = sqrt(pow((v2.getX() - v1.getX()), 2) + pow((v2.getY() - v1.getY()), 2)) +
-    sqrt(pow((v3.getX() - v2.getX()), 2) + pow((v3.getY() - v2.getY()), 2)) +
-    sqrt(pow((v3.getX() - v1.getX()), 2) + pow((v3.getY() - v1.getY()), 2));
+    double peri = abs(sqrt(pow((v2.getX() - v1.getX()), 2) + pow((v2.getY() - v1.getY()), 2))) +
+              abs(sqrt(pow((v3.getX() - v2.getX()), 2) + pow((v3.getY() - v2.getY()), 2))) +
+              abs(sqrt(pow((v3.getX() - v1.getX()), 2) + pow((v3.getY() - v1.getY()), 2)));
     return peri;
 }
 double Triangulo::area(){
